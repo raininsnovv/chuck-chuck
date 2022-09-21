91c84255319640c6f0a027448f499b3126903b32
raininsnovv <raininsnovv@gmail.com>
2022-09-22 02:33:50 +0300

    Необычные шутки Чака

---
+++ index.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".btn");
+const div = document.querySelector(".joke");
+
+const getJoke = async () => {
+  const img = document.createElement("img");
+  const responsive = await (
+    await fetch("https://api.chucknorris.io/jokes/random")
+  ).json();
+
+  console.log(responsive);
+  img.src = responsive.icon_url;
+  div.append(responsive.value, img);
+};
+
+btn.addEventListener("click", () => {
+  getJoke();
+  div.innerHTML = "";
+});
